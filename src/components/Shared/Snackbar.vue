a173aa0f9df348d847ee54e6869f1cf2e2ca1d6b
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-05-05 12:28:09 -0400

    Snack bar component added

---
+++ src/components/Shared/Snackbar.vue
@@ -0,0 +1,29 @@
+<template>
+   <v-snackbar
+      v-model="snackbar"
+      :multi-line="multiLine"
+    >
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="red"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
