d0115108e02e18f73775d3c0415e5bf728224ee1
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-05-05 00:13:10 -0400

    List Tasks component

---
+++ src/components/Todo/ListTasks.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-list
+      flat
+    >
+  <task
+    v-for="task in tasks"
+    :key="task.id"
+    :task="task"
+  />
+  </v-list>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  mounted() {
+      this.$store.dispatch('setTasks')
+      // Dispatch triggers the Action inside the Store
+    },
+  computed: {
+    ...mapState([
+      'tasks'
+    ]),
+  },
+  components: {
+    'task': require('@/components/Todo/Task.vue').default
+  }
+}
+</script>
+
+<style>
+
+</style>
